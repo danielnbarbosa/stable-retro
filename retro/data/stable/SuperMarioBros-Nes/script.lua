4100d90f1e3144523e9f5983a6af65bf0864829c
Daniel Barbosa <daniel@Mercurio.local>
2025-06-17 13:15:11 -0300

    SuperMarioBros-Nes v234

--- retro/data/stable/SuperMarioBros-Nes/script.lua
+++ retro/data/stable/SuperMarioBros-Nes/script.lua
@@ -3,32 +3,25 @@ previous_score = data.score
 previous_levelLo = data.levelLo
 previous_levelHi = data.levelHi
 previous_lives = data.lives
+is_dying = false
+stuck_counter = 0
 
 debug = false
 
 
 function xscrollLo_reward ()
-    if data.xscrollLo > previous_xscrollLo then
+    -- only give reward if ypos is above level of a pit
+    -- this avoids harvesting reward on big jumps into air
+    -- as seen in stages 1-3, 4-3 and 5-3
+    if data.xscrollLo > previous_xscrollLo and data.yposHi <= 1 and data.yposLo <= 176 then
         local delta = data.xscrollLo - previous_xscrollLo
         local reward = delta * 0.1
         previous_xscrollLo = data.xscrollLo
+        stuck_counter = 0
         if debug then print('xscrollLo_reward: ', reward) end
         return reward
     else
         previous_xscrollLo = data.xscrollLo
-        return 0
-    end
-end
-
-
-function score_reward ()
-    if data.score > previous_score then
-        local delta = data.score - previous_score
-        local reward = delta * 0.1
-        previous_score = data.score
-        if debug then print('score_reward: ', reward) end
-        return reward
-    else
         return 0
     end
 end
@@ -75,6 +68,89 @@ function lives_reward ()
 end
 
 
+function lives_tracker ()
+    if data.lives < previous_lives then
+        previous_lives = data.lives
+        is_dying = false
+    end
+    return 0
+end
+
+
+function dying_penalty ()
+    -- if hit by enemy
+    if data.state == 11 and is_dying == false then
+        local reward = -5
+        if debug then print('dying_penalty: ', reward) end
+        is_dying = true
+        return reward
+    -- if fall into pit
+    elseif data.state == 8 and data.yposHi == 1 and data.yposLo > 176 and is_dying == false then
+        local reward = -12.5
+        if debug then print('dying_penalty: ', reward) end
+        is_dying = true
+        return reward
+    else
+        return 0
+    end
+end
+
+
+function step_penalty ()
+    local reward = -0.02
+    stuck_counter = stuck_counter + 1
+    -- print(stuck_counter)
+    if debug then print('step_penalty: ', reward) end
+    return reward
+end
+
+
+function stuck_penalty ()
+    -- penalize if not making forward progress after some time
+    -- should help with checkpoints on 1-3, 4-3 and 5-3
+    -- as well as exit pipes in 2-2 and 7-2
+    if stuck_counter > 500 then
+        local reward = -0.1
+        if debug then print('stuck_penalty: ', reward) end
+        return reward
+    else
+        return 0
+    end
+end
+
+
+function sum_reward ()
+    return xscrollLo_reward() + levelLo_reward() + levelHi_reward() + lives_reward() + dying_penalty() + step_penalty() + lives_tracker()
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- UNUSED
+
+
+function score_reward ()
+    if data.score > previous_score then
+        local delta = data.score - previous_score
+        local reward = delta * 0.1
+        previous_score = data.score
+        if debug then print('score_reward: ', reward) end
+        return reward
+    else
+        return 0
+    end
+end
+
+
 function lives_penalty ()
     if data.lives < previous_lives then
         local delta = data.lives - previous_lives
@@ -97,28 +173,4 @@ function velocity_penalty ()
     else
         return 0
     end
-end
-
-
-function ypos_penalty ()
-    -- this acts like a dying penalty
-    if data.yposHi == 1 and data.yposLo >= 200 and data.yposLo < 204 then
-        local reward = -21
-        if debug then print('ypos_penalty: ', reward) end
-        return reward
-    else
-        return 0
-    end
-end
-
-
-function step_penalty ()
-    local reward = -0.02
-    if debug then print('step_penalty: ', reward) end
-    return reward
-end
-
-
-function sum_reward ()
-    return xscrollLo_reward() + score_reward() + levelLo_reward() + levelHi_reward() + lives_reward() + ypos_penalty() + step_penalty()
 end
